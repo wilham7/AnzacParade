832378f936a72749e38cb42929d233763f3857a7
BaptisteHiggs <16fogterminator@gmail.com>
2017-09-07 22:19:23 +1000

    Buttons work :O
    Absoloutely terrible code, but it works

--- scripts.js
+++ scripts.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
-    $("#demo").html("Hello, World!");
-	$('#b1').click(function(){
-
-		if($('#b1').attr('value')=='Hide'){
-			$(this).val('Show'); // change the value to Show
-			$('#d1').hide();
+	$('#buttonConstruction').click(function(){
+		if($('#buttonConstruction').attr('value')=='Construction On'){
+			$(this).val('Construction Off'); // change the value to Show
+			$("#Construction").hide();
+			$(this).show();
 		}else{
-			$(this).val('Hide'); // change the value to Hide
-		$('#d1').show();
+			$(this).val('Construction On'); // change the value to Hide
+			$("#Construction").show();
 		}
-	}
+	})
+	
+	$('#buttonEdges').click(function(){
+		if($('#buttonEdges').attr('value')=='Edges On'){
+			$(this).val('Edges Off'); // change the value to Show
+			$("#Edges").hide();
+			$(this).show();
+		}else{
+			$(this).val('Edges On'); // change the value to Hide
+			$("#Edges").show();
+		}
+	})
 });
